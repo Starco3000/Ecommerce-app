e7cf434b84e8f4f997c290bad6f63c3b3ee802df
Đỗ Hữu Thắng <84236168+thangfatocdo@users.noreply.github.com>
2023-11-25 00:46:19 +0700

    Update images.dart

--- lib/seller_screen/const/images.dart
+++ lib/seller_screen/const/images.dart
@@ -3,3 +3,4 @@ const icOrders = 'assets/icons/orders.png';
 const icGeneralSettings = 'assets/icons/general_setting.png';
 const icStar = 'assets/icons/star.png';
 const imgProduct = 'assets/images/product.jpg';
+const icShopSettings = "assets/icons/shop_setting.png";
