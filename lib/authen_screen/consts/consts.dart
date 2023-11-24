9506bbaa8365df516a4fadee25aba32560bdbdd8
Đỗ Hữu Thắng <84236168+thangfatocdo@users.noreply.github.com>
2023-11-24 22:47:41 +0700

    Delete lib/authen_screen directory

--- lib/authen_screen/consts/consts.dart
+++
@@ -1,8 +0,0 @@
-export './colors.dart';
-export './images.dart';
-export './strings.dart';
-export './styles.dart';
-export './lists.dart';
-export './firebase_consts.dart';
-export 'package:velocity_x/velocity_x.dart';
-export 'package:flutter/material.dart';
